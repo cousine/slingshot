b1ec3c24f7a528d372c3002d3478acd3a1b2d5fc
Karel Minarik <karmi@karmi.cz>
2011-02-16 18:08:58 +0100

    Added first part of Persistence layer: simple finders
    (Thanks @jordansissel for `{"query":{"terms": {"_id":["id1", "id2"]} }}`)

--- lib/slingshot.rb
+++ lib/slingshot.rb
@@ -1,5 +1,6 @@
 require 'rest_client'
 require 'yajl/json_gem'
+require 'active_model'
 
 require 'slingshot/rubyext/hash'
 require 'slingshot/configuration'
@@ -15,6 +16,7 @@ require 'slingshot/index'
 require 'slingshot/dsl'
 require 'slingshot/model/search'
 require 'slingshot/model/callbacks'
+require 'slingshot/model/persistence'
 
 module Slingshot
   extend DSL
